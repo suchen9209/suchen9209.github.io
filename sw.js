efd595629d91f755f8ed326f906107ac6731fa6c
such <suchen9209@126.com>
2021-10-11 10:31:36 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -57,3 +57,16 @@ self.addEventListener('notificationclick', function(event) {
   );
   // event.waitUntil确保浏览器不会在显示新窗口前终止服务工作线程
 });
+
+
+var title = 'Yay a message.';
+var body = 'We have received a push message.';
+var icon = '/images/icon-192x192.png';
+var tag = 'simple-push-demo-notification-tag';
+
+  self.registration.showNotification(title, {
+    body: body,
+    icon: icon,
+    tag: tag
+  })
+
